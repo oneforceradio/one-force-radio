90872f4132827389839e09fbf5194152e11c2aca
oneforceradio <oneforceradio@gmail.com>
2026-05-08 16:07:39 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,24 +9,32 @@ export default function Home() {
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        fontFamily: "Arial",
+        fontFamily: "Arial, sans-serif",
         padding: 20,
         textAlign: "center",
       }}
     >
-      <h1 style={{ fontSize: "48px", marginBottom: "10px" }}>
+      <h1 style={{ fontSize: "56px", marginBottom: "10px" }}>
         One Force Radio
       </h1>
 
-      <p style={{ marginBottom: "30px", color: "#aaa" }}>
+      <p style={{ marginBottom: "30px", color: "#aaa", fontSize: "20px" }}>
         Live 24/7 Dancehall • Reggae • Afrobeats
       </p>
 
-      <audio controls autoPlay style={{ width: "100%", maxWidth: "500px" }}>
+      <audio
+        controls
+        preload="none"
+        style={{
+          width: "100%",
+          maxWidth: "700px",
+        }}
+      >
         <source
-          src="https://sky.doscast.com/stream/oneforce/stream.pls"
+          src="https://sky.doscast.com/stream/oneforce"
           type="audio/mpeg"
         />
+        Your browser does not support the audio player.
       </audio>
     </div>
   );
