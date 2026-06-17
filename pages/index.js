bead5ab4bb73beba959be13596bd8f586efa6554
oneforceradio <oneforceradio@gmail.com>
2026-06-17 15:50:07 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -274,13 +274,15 @@ const [shoutouts, setShoutouts] = useState([]);
   link: "https://wa.me/393516232824",
 },
 
-    {
+ {
   title: "Stuck In The 90's",
   date: "27 June 2026",
   dateTime: "2026-06-27T23:00:00",
   location: "Supreme Lounge • Bremen, Germany",
   info: "A 90s throwback party featuring DJ Lashes (OneForce Sound) playing the best of 90s Dancehall, Old School Hip Hop, R&B Classics, Deutsch Rap, Trap and UK classics.",
-  flyers:
+  flyers: ["/events/bremen-27th-june.jpg"],
+  link: "https://wa.me/393516232824",
+},
 
 {
   title: "Dancehall Queen & King",
